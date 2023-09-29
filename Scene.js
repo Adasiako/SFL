295f171804033c7d4b36bbb0d2aea418fb6322b5
Adasiako <146480759+Adasiako@users.noreply.github.com>
2023-09-29 20:43:43 +0200

    Create Scene.js

---
+++ Scene.js
@@ -0,0 +1,13 @@
+class ExternalScene extends window.BaseScene {
+  constructor() {
+    super({
+      player: {
+       // The x/y coordinates on your Map where you want the player to appear
+        spawn: {
+          x: 210,
+          y: 280,
+        },
+      },
+    });
+  }
+}
